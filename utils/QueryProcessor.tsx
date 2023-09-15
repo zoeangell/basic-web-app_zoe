6b33d5014a289a088c7385b5ce6c3cbbcd7ecebe
Zoe Angell <you@example.com>
2023-09-15 12:37:22 -0400

    added andrewid query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,5 +7,11 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("andrewid")) {
+    return (
+      "Your andrewid is zangell"
+    );
+  }
+
   return "";
 }
